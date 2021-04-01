0c2dff3817386be87d395b2a2900e656e7c2a13e
guillaumeC76 <57393436+guillaumeC76@users.noreply.github.com>
2021-04-01 10:48:51 +0200

    Debut de l'ajout de la bdd

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -150,11 +150,6 @@ export default {
       //localStorage.clear();
     },
   },
-  mounted() {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
-  },
   watch: {
     timeLeft(newValue) {
       if (newValue === 0) {
